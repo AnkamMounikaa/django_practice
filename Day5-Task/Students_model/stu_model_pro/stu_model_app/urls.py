7b882f3d834dd1550e482fa471a5a20936dc44a5
AnkamMounikaa <apsmouni@gmail.com>
2025-10-22 12:02:45 +0530

    performing CRUD operations

--- Day5-Task/Students_model/stu_model_pro/stu_model_app/urls.py
+++ Day5-Task/Students_model/stu_model_pro/stu_model_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns=[
     path('get_details/', view=views.get_details),
-    path('details/',view=views.stu_details)
-    
+    path('details/',view=views.stu_details),
+    path('put_details/',view=views.updateuser),
+    path('del_details/',view=views.deleteuser)  
 ]
--- Day5-Task/Students_model/stu_model_pro/stu_model_app/views.py
+++ Day5-Task/Students_model/stu_model_pro/stu_model_app/views.py
@@ -3,13 +3,14 @@ from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Stundent_details
 
-
+#GET
 def get_details(request):
     if request.method=="GET":
         user=Stundent_details.objects.all()
         data=list(user.values())
         return JsonResponse({"data":list(data)})
 
+#POST
 @csrf_exempt
 def stu_details(request):
     if request.method=="POST":
@@ -21,3 +22,40 @@ def stu_details(request):
         details = Stundent_details.objects.create(first_name=user_fname,last_name=user_lname, age=user_age, roll_num=user_rollnum)
         details.save()
         return JsonResponse({"message": "Coder added successfully!", "coder_id": details.id})
+
+#PUT 
+@csrf_exempt
+def updateuser(req,id):
+
+    if req.method=='PUT':
+        try:
+            coder=Stundent_details.objects.get(id=id)
+
+            user_data=Stundent_details.loads(req.body)
+
+            idn=user_data["id"]
+            namen=user_data["name"]
+            agen=user_data["age"]
+
+            coder.id=idn
+            coder.name=namen
+            coder.age=agen
+            coder.save()
+            return HttpResponse("user updated succesfully")
+        
+        except:
+            return HttpResponse("user not found")
+
+
+#DELETE
+@csrf_exempt
+def deleteuser(req,id):
+    if req.method=='DELETE':
+        try:
+            coder=Stundent_details.objects.get(id=id)
+
+            coder.delete()
+            
+            return HttpResponse("user deleted succesfully")
+        except:
+            return HttpResponse("user not found")
